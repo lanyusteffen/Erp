f84b7eeba1d74a623ef38978a978cdf403427ebe
unknown <lanyusteffen@gmail.com>
2018-10-14 17:41:35 +0800

    邮箱验证逻辑有误

--- src/app/validators/email.valid.ts
+++ src/app/validators/email.valid.ts
@@ -2,7 +2,7 @@ import { FormControl } from '@angular/forms';
 export class EmailValid {
 
     public static validation(ctrl: FormControl) {
-        const REXEMAIL = new RegExp('^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$');
+        const REXEMAIL = new RegExp('[\\w]+@[a-zA-Z_]+\.[a-zA-Z]{2,3}$');
         return REXEMAIL.test(ctrl.value) ? null : {
             result: {
                 valid: false,
