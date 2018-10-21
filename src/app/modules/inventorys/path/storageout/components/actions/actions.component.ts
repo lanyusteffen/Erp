249721fc451a47206fec1547aefecedc54bbf247
breeze-gu <408898008@qq.com>
2018-10-21 15:21:18 +0800

    添加出库

---
+++ src/app/modules/inventorys/path/storageout/components/actions/actions.component.ts
@@ -0,0 +1,69 @@
+﻿import { Component, Input } from '@angular/core';
+import { StorageOutService } from '../../storageout.service';
+import { ConfirmService } from '@services/confirm.service';
+import { AlertService, ModuleName } from '@services/alert.service';
+import { TabsService } from '@components/tabs/tabs.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-storageout-actions',
+  templateUrl: './actions.component.html',
+  styleUrls: ['./actions.component.less']
+})
+
+export class StorageOutActionsComponent {
+  private _show = false;
+  private selectedId: number;
+
+  @Input() selectedItems = <any>[];
+
+  constructor(
+    private storageOutService: StorageOutService,
+    private confirmService: ConfirmService,
+    private alertService: AlertService,
+    private tabsService: TabsService,
+	private router: Router
+  ) { }
+
+  show() {
+    this._show = true;
+    this.selectedId = 0;
+  }
+
+  showDisabled() {
+    this.tabsService.create({
+      name: '停用出库单',
+      link: '/inventorys/storageout/disabled'
+    });
+	this.router.navigate(['/inventorys/storageout/disabled']);
+  }
+
+  close() {
+    this._show = false;
+  }
+
+  onSearch(queryKey) {
+    this.storageOutService.onSearch(queryKey, (err) => {
+      this.alertService.listErrorCallBack(ModuleName.StorageOut, err);
+    });
+  }
+
+  onCancel() {
+    this.confirmService.open({
+      content: '确认删除吗？',
+      onConfirm: () => {
+        this.storageOutService
+          .cancel(this.selectedItems.map(item => item.Id), data => {
+            if (data.IsValid) {
+              this.alertService.removeSuccess();
+              this.storageOutService.list((err) => {
+                this.alertService.listErrorCallBack(ModuleName.StorageOut, err);
+              });
+            }
+          }, (err) => {
+            this.alertService.removeFail(err);
+          });
+      }
+    });
+  }
+}
---
+++ src/app/modules/inventorys/path/storageout/storageout.module.ts
@@ -0,0 +1,42 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { UIModule } from '@UI/ui.module';
+
+import { StorageOutComponent } from './storageout.component';
+import { StorageOutListComponent } from './components/list/list.component';
+import { StorageOutActionsComponent } from './components/actions/actions.component';
+import { StorageOutControlComponent } from './components/control/control.component';
+import { StorageOutDisabledComponent } from './storageout-disabled.component';
+import { StorageOutDisabledListComponent } from './components/disabled/disabled.component';
+
+import { StorageOutService } from './storageout.service';
+import { SharedModule } from '@app/shared.module';
+import { StorageOutRoutingModule } from './storageout.router';
+
+import { SimpleReuseStrategy } from '@strategies/SimpleReuseStrategy';
+
+@NgModule({
+  declarations: [
+    StorageOutComponent,
+    StorageOutListComponent,
+    StorageOutActionsComponent,
+    StorageOutControlComponent,
+    StorageOutDisabledComponent,
+    StorageOutDisabledListComponent
+  ],
+  imports: [
+    UIModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    StorageOutRoutingModule
+  ],
+  providers: [ StorageOutService ]
+})
+
+export class StorageOutModule {
+}
