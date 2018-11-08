3f796d566e83a1cb07230f3a9745ee50afc61a70
lanyusteffen <lanyusteffen@gmail.com>
2018-11-08 08:53:15 +0800

    屏蔽报错代码

--- src/app/modules/inventorys/path/storageout/storageout.module.ts
+++ src/app/modules/inventorys/path/storageout/storageout.module.ts
@@ -1,32 +1,32 @@
-﻿import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouteReuseStrategy } from '@angular/router';
+﻿// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { RouteReuseStrategy } from '@angular/router';
 
-import { UIModule } from '@UI/ui.module';
+// import { UIModule } from '@UI/ui.module';
 
-import { StorageOutComponent } from './storageout.component';
+// import { StorageOutComponent } from './storageout.component';
 
-import { StorageOutService } from './storageout.service';
-import { SharedModule } from '@app/shared.module';
-import { StorageOutRoutingModule } from './storageout.router';
+// import { StorageOutService } from './storageout.service';
+// import { SharedModule } from '@app/shared.module';
+// import { StorageOutRoutingModule } from './storageout.router';
 
-import { SimpleReuseStrategy } from '@strategies/SimpleReuseStrategy';
+// import { SimpleReuseStrategy } from '@strategies/SimpleReuseStrategy';
 
-@NgModule({
-  declarations: [
-    StorageOutComponent
-  ],
-  imports: [
-    UIModule,
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    SharedModule,
-    StorageOutRoutingModule
-  ],
-  providers: [ StorageOutService ]
-})
+// @NgModule({
+//   declarations: [
+//     StorageOutComponent
+//   ],
+//   imports: [
+//     UIModule,
+//     CommonModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//     SharedModule,
+//     StorageOutRoutingModule
+//   ],
+//   providers: [ StorageOutService ]
+// })
 
-export class StorageOutModule {
-}
+// export class StorageOutModule {
+// }
--- src/app/modules/inventorys/path/storageout/storageout.router.ts
+++ src/app/modules/inventorys/path/storageout/storageout.router.ts
@@ -1,20 +1,20 @@
-﻿import { StorageOutDisabledComponent } from './storageout-disabled.component';
-import { Routes, RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { StorageOutComponent } from './storageout.component';
+﻿// import { StorageOutDisabledComponent } from './storageout-disabled.component';
+// import { Routes, RouterModule } from '@angular/router';
+// import { NgModule } from '@angular/core';
+// import { StorageOutComponent } from './storageout.component';
 
-export const ROUTES: Routes = [{
-  path: '',
-  children: [
-    { path: '', component: StorageOutComponent },
-    { path: 'disabled', component: StorageOutDisabledComponent }
-  ]
-}];
+// export const ROUTES: Routes = [{
+//   path: '',
+//   children: [
+//     { path: '', component: StorageOutComponent },
+//     { path: 'disabled', component: StorageOutDisabledComponent }
+//   ]
+// }];
 
-@NgModule({
-  imports: [RouterModule.forChild(ROUTES)],
-  exports: [RouterModule]
-})
+// @NgModule({
+//   imports: [RouterModule.forChild(ROUTES)],
+//   exports: [RouterModule]
+// })
 
-export class StorageOutRoutingModule {
-}
+// export class StorageOutRoutingModule {
+// }
