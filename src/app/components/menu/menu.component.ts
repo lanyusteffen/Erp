61368199f3392922af1fb058eb481c58661d2fde
lanyusteffen <lanyusteffen@gmail.com>
2019-04-30 10:22:38 +0800

    菜单调整

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -13,80 +13,9 @@ export class AppMenuComponent {
     private router: Router) {}
   menus = [
     {
-      name: '采购',
-      icon: 'cart',
-      subMenu: [
-        {
-          name: '采购订单管理',
-          subMenu: [
-            { name: '新增采购订单', link: '/purchases/order', paras: { type: 'new', id: -1 }},
-            { name: '采购订单历史', link: '/purchases/order/history' }
-          ]
-        }
-      ]
-    },
-    {
-      name: '销售',
-      icon: 'manageorder',
-      subMenu: [
-        {
-          name: '销售订单管理',
-          subMenu: [
-            { name: '新增销售订单', link: '/sales/order', paras: { type: 'new', id: -1 }},
-            { name: '销售订单历史', link: '/sales/order/history' }
-          ]
-        }
-      ]
-    },
-    {
-      name: '库存',
-      icon: 'box',
-      subMenu: [
-        {
-          name: '出库单管理',
-          subMenu: [
-            { name: '新增出库单', link: '/inventorys/storageout', paras: { type: 'new', id: -1 } },
-            { name: '出库单历史', link: '/inventorys/storageout/history' }
-          ]
-        }
-      ]
-    },
-    {
-      name: '财务',
-      icon: 'dollar',
-      subMenu: [
-        {
-          name: '基础资料',
-          subMenu: [
-            { name: '资金账户', link: '/finances/fundsaccount' },
-            { name: '费用类型', link: '/finances/feetype' },
-            { name: '收入类型', link: '/finances/otherincome' }
-          ]
-        }
-      ]
-    },
-    {
-      name: '报表',
-      icon: 'training',
-      subMenu: [
-        {
-          name: '商品管理',
-          subMenu: [
-          ]
-        }
-      ]
-    },
-    {
       name: '资料',
       icon: 'data',
       subMenu: [
-        {
-          name: '商品管理',
-          subMenu: [
-            { name: '商品信息', link: '/products/product' },
-            { name: '商品配置', link: '/products/productconfig' }
-          ]
-        },
         {
           name: '往来单位',
           subMenu: [
@@ -107,29 +36,88 @@ export class AppMenuComponent {
       ]
     },
     {
-      name: '设置',
-      icon: 'set',
+      name: '财务',
+      icon: 'dollar',
       subMenu: [
         {
-          name: '系统设置',
+          name: '基础资料',
           subMenu: [
-            { name: '公司管理', link: '/admins/company' },
-            { name: '用户管理', link: '/admins/user' },
-            { name: '角色管理', link: '/admins/role' },
-            { name: '系统配置', link: '/admins/systemconfig' }
+            { name: '资金账户', link: '/finances/fundsaccount' },
+            { name: '费用类型', link: '/finances/feetype' },
+            { name: '收入类型', link: '/finances/otherincome' }
+          ]
+        }
+      ]
+    },
+    {
+      name: '商品',
+      icon: 'similarproduct',
+      subMenu: [
+        {
+          name: '商品管理',
+          subMenu: [
+            { name: '商品信息', link: '/products/product' },
+            { name: '商品配置', link: '/products/productconfig' }
+          ]
+        }
+      ]
+    },
+    {
+      name: '销售',
+      icon: 'manageorder',
+      subMenu: [
+        {
+          name: '销售订单管理',
+          subMenu: [
+            { name: '新增销售订单', link: '/sales/order', paras: { type: 'new', id: -1 }},
+            { name: '销售订单历史', link: '/sales/order/history' }
+          ]
+        }
+      ]
+    },
+    {
+      name: '采购',
+      icon: 'cart',
+      subMenu: [
+        {
+          name: '采购订单管理',
+          subMenu: [
+            { name: '新增采购订单', link: '/purchases/order', paras: { type: 'new', id: -1 }},
+            { name: '采购订单历史', link: '/purchases/order/history' }
+          ]
+        }
+      ]
+    },
+    {
+      name: '库存',
+      icon: 'box',
+      subMenu: [
+        {
+          name: '出库单管理',
+          subMenu: [
+            { name: '新增出库单', link: '/inventorys/storageout', paras: { type: 'new', id: -1 } },
+            { name: '出库单历史', link: '/inventorys/storageout/history' }
           ]
         }
       ]
     },
     {
       name: '管理',
-      icon: 'information',
+      icon: 'set',
       subMenu: [
         {
-          name: '权限管理',
+          name: '基础资料',
           subMenu: [
-            { name: '菜单', link: '/admins/menu' },
-            { name: '操作', link: '/admins/operation' }
+            { name: '公司管理', link: '/admins/company' },
+            { name: '用户管理', link: '/admins/user' },
+            { name: '角色管理', link: '/admins/role' }
+          ]
+        },
+        {
+          name: '系统设置',
+          subMenu: [
+            { name: '菜单设置', link: '/admins/menu' },
+            { name: '全局配置', link: '/admins/systemconfig' }
           ]
         }
       ]
